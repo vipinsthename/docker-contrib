a1f28c5839801f9a0ca86b8a0fa32cda41f3cb2a
Vipin Nair <v.raveendran.nair@acp.aws.accenture.com>
2015-02-18 11:54:09 +0000

    Script to generate base centos/redhat image

--- create_centos_img.sh
+++ create_centos_img.sh
@@ -6,67 +6,63 @@
 # a CentOS image on CentOS).  See contrib/mkimage-rinse.sh for a way
 # to build CentOS images on other systems.
 
-#usage() {
-#    cat <<EOOPTS
-#$(basename $0) [OPTIONS] <name>
-#OPTIONS:
-#  -y <yumconf>  The path to the yum config to install packages from. The
-#                default is /etc/yum.conf.
-#EOOPTS
-#    exit 1
-#}
+usage() {
+  cat <<EOOPTS
+$(basename $0) [OPTIONS] <name>
+OPTIONS:
+  -y <yumconf>    The path to the yum config to install packages from. The
+                  default is /etc/yum.conf.
+  -d <directory>  The path where the image should be created. 
+				  Make sure that it has enough space.
+  -h <help>  Displays this help.
+EOOPTS
+  exit 1
+}
 
-#option defaults
-yum_config=/etc/yum.conf
-#while getopts ":y:h" opt; do
-#    case $opt in
-#        y)
-#            yum_config=$OPTARG
-#            echo $yum_config
-#            ;;
-#        h)
-#            usage
-#            ;;
-#        \?)
-#            echo "Invalid option: -$OPTARG"
-#            usage
-#            ;;
-#   esac
-#done
-#shift $((OPTIND - 1))
-name=$1
-#yum_config=$1
-echo "NAME --> $name --<"
+while getopts ":y:d:h" o; do
+    case "${o}" in
+        y)
+            yum_config=${OPTARG}
+            ;;
+        d)
+            directory=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 
-if [[ -z $name ]]; then
+if [ -z "${yum_config}" ] || [ -z "${directory}" ]; then
     usage
 fi
 
-#--------------------
+echo "Changing directory to ${directory}"
+target=$(mktemp -d ${directory}/$(basename $0).XXXXXX)
 
-target=$(mktemp -d $(basename $0).XXXXXX)
+echo "Yum config provided is ${yum_config}"
 
-echo "TARGET --> $target <--"
+#
+# The below part of the script was taken from 
+# https://github.com/docker/docker/blob/master/contrib/mkimage-yum.sh and tweaked
+#
+mkdir -m 755 "$target"/dev
+mknod -m 600 "$target"/dev/console c 5 1
+mknod -m 600 "$target"/dev/initctl p
+mknod -m 666 "$target"/dev/full c 1 7
+mknod -m 666 "$target"/dev/null c 1 3
+mknod -m 666 "$target"/dev/ptmx c 5 2
+mknod -m 666 "$target"/dev/random c 1 8
+mknod -m 666 "$target"/dev/tty c 5 0
+mknod -m 666 "$target"/dev/tty0 c 4 0
+mknod -m 666 "$target"/dev/urandom c 1 9
+mknod -m 666 "$target"/dev/zero c 1 5
 
-set -x
-
-mkdir -m 755 "/tmp/$target"/dev
-mknod -m 600 "/tmp/$target"/dev/console c 5 1
-mknod -m 600 "/tmp/$target"/dev/initctl p
-mknod -m 666 "/tmp/$target"/dev/full c 1 7
-mknod -m 666 "/tmp/$target"/dev/null c 1 3
-mknod -m 666 "/tmp/$target"/dev/ptmx c 5 2
-mknod -m 666 "/tmp/$target"/dev/random c 1 8
-mknod -m 666 "/tmp/$target"/dev/tty c 5 0
-mknod -m 666 "/tmp/$target"/dev/tty0 c 4 0
-mknod -m 666 "/tmp/$target"/dev/urandom c 1 9
-mknod -m 666 "/tmp/$target"/dev/zero c 1 5
-
-rpm --dbpath "/tmp/$target" --initdb
-yum -c "$yum_config" --installroot="/tmp/$target" -y install glibc-devel.i386
-yum -c "$yum_config" --installroot="/tmp/$target" \
-    -y groupinstall Core Base 'Development Tools'
-yum -c "$yum_config" --installroot="/tmp/$target" -y clean all
+rpm --dbpath "$target" --initdb
+yum -c "$yum_config" --installroot="$target" \
+    -y groupinstall Core Base
+yum -c "$yum_config" --installroot="$target" -y clean all
 
 cat > "$target"/etc/sysconfig/network <<EOF
 NETWORKING=yes
@@ -76,22 +72,22 @@ EOF
 # effectively: febootstrap-minimize --keep-zoneinfo --keep-rpmdb
 # --keep-services "$target".  Stolen from mkimage-rinse.sh
 #  locales
-rm -rf "/tmp/$target"/usr/{{lib,share}/locale,{lib,lib64}/gconv,bin/localedef,sbin/build-locale-archive}
+rm -rf "$target"/usr/{{lib,share}/locale,{lib,lib64}/gconv,bin/localedef,sbin/build-locale-archive}
 #  docs
-rm -rf "/tmp/$target"/usr/share/{man,doc,info,gnome/help}
+rm -rf "$target"/usr/share/{man,doc,info,gnome/help}
 #  cracklib
-rm -rf "/tmp/$target"/usr/share/cracklib
+rm -rf "$target"/usr/share/cracklib
 #  i18n
-rm -rf "/tmp/$target"/usr/share/i18n
+rm -rf "$target"/usr/share/i18n
 #  sln
-rm -rf "/tmp/$target"/sbin/sln
+rm -rf "$target"/sbin/sln
 #  ldconfig
-rm -rf "/tmp/$target"/etc/ld.so.cache
-rm -rf "/tmp/$target"/var/cache/ldconfig/*
+rm -rf "$target"/etc/ld.so.cache
+rm -rf "$target"/var/cache/ldconfig/*
 
 version=
-if [ -r "/tmp/$target"/etc/redhat-release ]; then
-    version="$(sed 's/^[^0-9\]*\([0-9.]\+\).*$/\1/' "/tmp/$target"/etc/redhat-release)"
+if [ -r "$target"/etc/redhat-release ]; then
+    version="$(sed 's/^[^0-9\]*\([0-9.]\+\).*$/\1/' "$target"/etc/redhat-release)"
 fi
 
 if [ -z "$version" ]; then
@@ -99,6 +95,5 @@ if [ -z "$version" ]; then
     version=$name
 fi
 
-tar --numeric-owner -cf "${target}.tar" -C "/tmp/$target" .
-#| docker import - $name:$version
-#docker run -i -t $name:$version echo success
+echo "The tar ball would be created in ${directory}"
+tar --numeric-owner -cf "${target}.tar" -C "$target" .
